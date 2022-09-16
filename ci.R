3d2f20e4a088c0affbd1456025a7d97280a3a5b8
Florent Chuffart <florent.chuffart@gmail.com>
2022-09-16 15:50:00 +0200

    Sort and Update

--- ci.R
+++ ci.R
@@ -1,5 +1,7 @@
-rmarkdown::render("tp_meth.Rmd")
-rmarkdown::render("tp_meth.Rmd")
-rmarkdown::render("tp_biodiv.Rmd")
-rmarkdown::render("tp_bdf.Rmd")
 rmarkdown::render("tp1.Rmd")
+rmarkdown::render("tp1.2.Rmd")
+rmarkdown::render("tp2.Rmd")
+rmarkdown::render("tp3.Rmd")
+rmarkdown::render("tp_meth.Rmd")
+# rmarkdown::render("tp_biodiv.Rmd")
+

0c14ed05078620aae796e094bb7c4553b3578c1b
Florent Chuffart <florent.chuffart@gmail.com>
2022-09-16 16:02:10 +0200

    Adding TPs

--- ci.R
+++ ci.R
@@ -3,5 +3,5 @@ rmarkdown::render("tp1.2.Rmd")
 rmarkdown::render("tp2.Rmd")
 rmarkdown::render("tp3.Rmd")
 rmarkdown::render("tp_meth.Rmd")
-# rmarkdown::render("tp_biodiv.Rmd")
+rmarkdown::render("tp_biodiv.Rmd")
 
